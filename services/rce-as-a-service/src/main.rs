1f28d145dfe2f1fffe6e4b3f4bc298c9ad0d5f05
Alexey Tarasov <tarasovion2004@gmail.com>
2025-02-14 01:05:58 +0300

    gggg

--- services/rce-as-a-service/src/main.rs
+++ services/rce-as-a-service/src/main.rs
@@ -1,133 +1,29 @@
 mod auth;
 mod cleaner;
 mod misc;
+mod routes;
 mod sandbox;
 mod storage;
 
 use crate::auth::Authenticator;
-use crate::sandbox::ExecutionResult;
+use crate::routes::{execute, login};
 use crate::sandbox::Sandbox;
-use base64::prelude::BASE64_STANDARD;
-use base64::Engine as _;
 use cleaner::Cleaner;
-use rocket::http::{Cookie, CookieJar, Status};
-use rocket::request::{FromRequest, Outcome};
-use rocket::response::status;
-use rocket::serde::{json::Json, Deserialize};
-use rocket::{launch, post, routes, Request, State};
+use rand::TryRngCore;
+use rocket::config::SecretKey;
+use rocket::{launch, routes};
 use std::net::{IpAddr, Ipv4Addr};
+use std::path::PathBuf;
 use std::sync::Arc;
 use std::time::Duration;
 use storage::Storage;
 
-const COOKIE_NAME: &str = "username";
-
-struct AuthenticatedUser(pub String);
-
-#[rocket::async_trait]
-impl<'r> FromRequest<'r> for AuthenticatedUser {
-    type Error = String;
-
-    async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
-        let respond_with = |status: Status| -> Outcome<Self, Self::Error> {
-            Outcome::Error((status, status.reason_lossy().to_string()))
-        };
-
-        let cookies = request.cookies();
-        let username = match cookies.get_private(COOKIE_NAME) {
-            Some(cookie) => cookie.value().to_string(),
-            None => {
-                return respond_with(Status::Unauthorized);
-            }
-        };
-
-        let auth = match request
-            .guard::<&State<Arc<Authenticator>>>()
-            .await
-            .succeeded()
-        {
-            Some(auth) => auth.inner(),
-            None => {
-                return respond_with(Status::ServiceUnavailable);
-            }
-        };
-
-        let exists = auth.exists(&username).await;
-        match exists {
-            Ok(exists) => {
-                if !exists {
-                    respond_with(Status::Unauthorized)
-                } else {
-                    Outcome::Success(AuthenticatedUser(username))
-                }
-            }
-            Err(_) => respond_with(Status::ServiceUnavailable),
-        }
-    }
-}
-
-#[derive(Deserialize)]
-#[serde(crate = "rocket::serde")]
-struct ExecutionRequest<'r> {
-    wasm: &'r str,
-    args: Vec<String>,
-}
-
-#[post("/execute", format = "plain", data = "<data>")]
-async fn execute(
-    data: Json<ExecutionRequest<'_>>,
-    authenticated_user: AuthenticatedUser,
-    sandbox: &State<Sandbox>,
-) -> std::result::Result<Json<ExecutionResult>, status::BadRequest<String>> {
-    let base64_wasm = data.wasm.as_bytes();
-    let wasm = BASE64_STANDARD
-        .decode(base64_wasm)
-        .map_err(|e| status::BadRequest(e.to_string()))?;
-
-    let result = sandbox
-        .execute_sandboxed(&authenticated_user.0, &wasm, &data.args)
-        .await
-        .map_err(|e| status::BadRequest(e.to_string()))?;
-
-    Ok(Json(result))
-}
-
-#[derive(Deserialize)]
-#[serde(crate = "rocket::serde")]
-struct LoginData<'r> {
-    username: &'r str,
-    password: &'r str,
-}
-
-#[post("/login", format = "json", data = "<data>")]
-async fn login(
-    data: Json<LoginData<'_>>,
-    auth: &State<Arc<Authenticator>>,
-    cookies: &CookieJar<'_>,
-) -> std::result::Result<String, status::Forbidden<String>> {
-    let authenticated = auth
-        .authenticate(&data.username, &data.password)
-        .await
-        .map_err(|e| status::Forbidden(e.to_string()))?;
-
-    if !authenticated {
-        return Err(status::Forbidden(
-            "Invalid username or password".to_string(),
-        ));
-    }
-
-    let cookie = Cookie::new(COOKIE_NAME, data.username.to_string());
-    cookies.add_private(cookie);
-
-    Ok("".to_string())
-}
-
 async fn get_components() -> (Arc<Storage>, Arc<Authenticator>, Sandbox) {
-    let s3_url = std::env::var("S3_URL").expect("S3_URL is set");
+    let s3_url = std::env::var("S3_URL").expect("S3_URL should be set");
     let storage = Arc::new(Storage::new(s3_url));
     let sandbox = Sandbox::new(storage.clone()).unwrap();
 
-    let db_url = std::env::var("DB_URL").expect("DB_URL is set");
+    let db_url = std::env::var("DB_URL").expect("DB_URL should be set");
     let auth = Arc::new(Authenticator::new(&db_url).await.unwrap());
 
     (storage, auth, sandbox)
@@ -137,24 +33,49 @@ async fn launch_cleaner(auth: Arc<Authenticator>, storage: Arc<Storage>) {
     let cleaner_interval = std::env::var("CLEANER_INTERVAL_MINS")
         .unwrap_or("1".to_string())
         .parse::<u64>()
-        .expect("CLEANER_INTERVAL_MINS is set to a valid integer");
+        .expect("CLEANER_INTERVAL_MINS should be set to a valid integer");
     let user_lifetime = std::env::var("USER_LIFETIME_MINS")
         .unwrap_or("20".to_string())
         .parse::<u64>()
-        .expect("USER_LIFETIME_MINS is set to a valid integer");
+        .expect("USER_LIFETIME_MINS should be set to a valid integer");
 
     let cleaner = Cleaner::new(auth.clone(), storage.clone(), user_lifetime)
         .await
-        .unwrap();
+        .expect("Cleaner should be initialized");
     tokio::spawn(async move {
         loop {
-            cleaner.clean().await.unwrap(); // TODO: handle all errors
             tokio::time::sleep(Duration::from_secs(
                 (60 * cleaner_interval).try_into().unwrap(),
             ))
             .await;
+            cleaner.clean().await.unwrap(); // TODO: handle all errors
         }
     });
+}
+
+fn generate_new_key(path: PathBuf) -> SecretKey {
+    let mut data = [0u8; 64];
+    rand::rngs::OsRng
+        .try_fill_bytes(&mut data)
+        .expect("Should be able to generate a new key");
+    std::fs::write(path, data).expect("Should be able to save a new key");
+    SecretKey::from(data.as_slice())
+}
+
+fn get_secret_key() -> SecretKey {
+    let secret_key_path = std::env::var("SECRET_KEY_PATH").expect("SECRET_KEY_PATH should be set");
+
+    if !std::path::Path::new(&secret_key_path).exists() {
+        return generate_new_key(secret_key_path.into());
+    }
+
+    let secret_key =
+        std::fs::read(&secret_key_path).expect("SECRET_KEY_PATH should be a valid file");
+    if secret_key.len() == 0 {
+        return generate_new_key(secret_key_path.into());
+    }
+
+    SecretKey::from(secret_key.as_slice())
 }
 
 #[launch]
@@ -162,9 +83,12 @@ async fn rocket() -> _ {
     let mut config = rocket::Config::default();
     config.address = IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0));
     config.port = 9091;
+    config.secret_key = get_secret_key();
 
     let (storage, auth, sandbox) = get_components().await;
+
     launch_cleaner(auth.clone(), storage.clone()).await;
+
     rocket::build()
         .manage(sandbox)
         .manage(auth)
---
+++ services/rce-as-a-service/src/routes.rs
@@ -0,0 +1,113 @@
+use crate::auth::Authenticator;
+use crate::sandbox::ExecutionResult;
+use crate::sandbox::Sandbox;
+use base64::prelude::BASE64_STANDARD;
+use base64::Engine as _;
+use rocket::http::{Cookie, CookieJar, Status};
+use rocket::request::{FromRequest, Outcome};
+use rocket::response::status;
+use rocket::serde::{json::Json, Deserialize};
+use rocket::{post, Request, State};
+use std::sync::Arc;
+
+const COOKIE_NAME: &str = "username";
+
+pub struct AuthenticatedUser(pub String);
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for AuthenticatedUser {
+    type Error = String;
+
+    async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        let respond_with = |status: Status| -> Outcome<Self, Self::Error> {
+            Outcome::Error((status, status.reason_lossy().to_string()))
+        };
+
+        let cookies = request.cookies();
+        let username = match cookies.get_private(COOKIE_NAME) {
+            Some(cookie) => cookie.value().to_string(),
+            None => {
+                return respond_with(Status::Unauthorized);
+            }
+        };
+
+        let auth = match request
+            .guard::<&State<Arc<Authenticator>>>()
+            .await
+            .succeeded()
+        {
+            Some(auth) => auth.inner(),
+            None => {
+                return respond_with(Status::ServiceUnavailable);
+            }
+        };
+
+        let exists = auth.exists(&username).await;
+        match exists {
+            Ok(exists) => {
+                if !exists {
+                    respond_with(Status::Unauthorized)
+                } else {
+                    Outcome::Success(AuthenticatedUser(username))
+                }
+            }
+            Err(_) => respond_with(Status::ServiceUnavailable),
+        }
+    }
+}
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct ExecutionRequest<'r> {
+    wasm: &'r str,
+    args: Vec<String>,
+}
+
+#[post("/execute", format = "plain", data = "<data>")]
+pub async fn execute(
+    data: Json<ExecutionRequest<'_>>,
+    authenticated_user: AuthenticatedUser,
+    sandbox: &State<Sandbox>,
+) -> std::result::Result<Json<ExecutionResult>, status::BadRequest<String>> {
+    let base64_wasm = data.wasm.as_bytes();
+    let wasm = BASE64_STANDARD
+        .decode(base64_wasm)
+        .map_err(|e| status::BadRequest(e.to_string()))?;
+
+    let result = sandbox
+        .execute_sandboxed(&authenticated_user.0, &wasm, &data.args)
+        .await
+        .map_err(|e| status::BadRequest(e.to_string()))?;
+
+    Ok(Json(result))
+}
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct LoginData<'r> {
+    username: &'r str,
+    password: &'r str,
+}
+
+#[post("/login", format = "json", data = "<data>")]
+pub async fn login(
+    data: Json<LoginData<'_>>,
+    auth: &State<Arc<Authenticator>>,
+    cookies: &CookieJar<'_>,
+) -> std::result::Result<String, status::Forbidden<String>> {
+    let authenticated = auth
+        .authenticate(&data.username, &data.password)
+        .await
+        .map_err(|e| status::Forbidden(e.to_string()))?;
+
+    if !authenticated {
+        return Err(status::Forbidden(
+            "Invalid username or password".to_string(),
+        ));
+    }
+
+    let cookie = Cookie::new(COOKIE_NAME, data.username.to_string());
+    cookies.add_private(cookie);
+
+    Ok("".to_string())
+}
